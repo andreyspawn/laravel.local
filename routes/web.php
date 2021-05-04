7bc79c00cbbc4251807c89187478d50964d170d1
andreyspawn <andrey.spawn@gmail.com>
2021-05-04 20:49:16 +0300

    04-05-2021 - начало сбора шаблона и менюшек

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 //
 //Auth::routes();
 //
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'Admin\HomeController@admin')->name('admin');
 
 Route::get('/blog','HomeController@indexBlog');
 
