35408bb761a2b0d0c52674950732df61a20736ee
andreyspawn <andrey.spawn@gmail.com>
2021-05-12 04:00:39 +0300

    11-05-2021 - добавил контроллер категорий

--- public/js/admin.js
+++ public/js/admin.js
@@ -3082,8 +3082,7 @@ $(document).ready(function ()
     });
 
     //iCheck for checkbox and radio inputs
-    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-        checkboxClass: 'icheckbox_minimal-blue',
-        radioClass: 'iradio_minimal-blue'
-    });
+    $('input[type="checkbox"].minimal').iCheck({
+         checkboxClass: 'icheckbox_minimal-blue'
+     });
 })
